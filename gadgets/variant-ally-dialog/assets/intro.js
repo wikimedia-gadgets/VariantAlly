0ff6f039c8bc26238114cb14f4ddda49855cb598
psnbaotg <psnbaotg@protonmail.com>
2023-12-14 22:51:04 +0800

    Update banner

--- gadgets/variant-ally-dialog/assets/intro.js
+++ gadgets/variant-ally-dialog/assets/intro.js
@@ -6,9 +6,9 @@
  * |  Please discuss changes on the talk page, [[WP:VPT]] or GitHub before editing.  |
  * |_________________________________________________________________________________|
  *
- * Built from GitHub repository (https://github.com/diskdance/VariantAlly), you should not make
+ * Built from GitHub repository (https://github.com/wikimedia-gadgets/VariantAlly), you should not make
  * changes directly here.
  *
- * See https://github.com/diskdance/VariantAlly/blob/main/CONTRIBUTING.md for build instructions.
+ * See https://github.com/wikimedia-gadgets/VariantAlly/blob/main/CONTRIBUTING.md for build instructions.
  */
 // <nowiki>
