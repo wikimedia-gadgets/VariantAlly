e0044f4305947e557d0e655a65688dad2b44e01f
psnbaotg <psnbaotg@protonmail.com>
2025-11-11 17:17:02 +0800

    Rename 'optout' to 'opt-out'

--- gadgets/variant-ally-dialog/src/App.vue
+++ gadgets/variant-ally-dialog/src/App.vue
@@ -27,7 +27,7 @@ addEventListener(isMobile ? 'touchmove' : 'scroll', () => {
 });
 
 function onOptOut() {
-  stat(`dialog_${isMobile ? 'mobile' : 'desktop'}`, 'optout');
+  stat(`dialog_${isMobile ? 'mobile' : 'desktop'}`, 'opt-out');
   setOptOut();
 }
 
